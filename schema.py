4425d7c9f1437bdd9f0ac1b4fed86c45e87c6129
choke06 <colehoke1@gmail.com>
2026-04-29 01:08:08 -0400

    Delete schema.py

--- schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Dict, Optional
-
-class ProductExtraction(BaseModel):
-    model_name: Optional[str] = None
-    brand: Optional[str] = None
-    additional_specs: Dict[str, str] = Field(default_factory=dict)
